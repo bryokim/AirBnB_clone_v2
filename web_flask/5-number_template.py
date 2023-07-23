d4e2f3ee66aeceb232c79c72e5822483f99d00ab
Brian Kimathi <bryo.kim1@gmail.com>
2023-07-23 22:01:32 +0300

    Added README.md

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application listening on 0.0.0.0 port 5000.
+"""
 
 from flask import Flask, render_template
 from markupsafe import escape
---
+++ web_flask/README.md
@@ -0,0 +1,4 @@
+# web_flask
+
+Contains web applications written in Flask.
+All run on 0.0.0.0 port 5000.
