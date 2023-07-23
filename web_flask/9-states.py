be16b18f9e7be5a80b7763532525ab4beab727b2
Brian Kimathi <bryo.kim1@gmail.com>
2023-07-24 01:01:45 +0300

    Both /states and /states/<id> are served by the same function.

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,7 +3,7 @@
 Starts a Flask web application listening on 0.0.0.0 port 5000.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from models import storage
 from models.state import State
@@ -12,47 +12,40 @@ app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-def states():
-    """Handles the get request for /states.
-
-    Returns:
-        str: Return from the render_template method.
-    """
-    return render_template(
-        "9-states.html",
-        states=storage.all(State).values()
-    )
-
-
 @app.route("/states/<id>", strict_slashes=False)
-def states_with_id(id):
-    """Handles the get request for /states/<id>.
-
-    Returns:
-        str: Return from the render_template method.
-    """
-    found = None
-
-    for state in storage.all(State).values():
-        if state.id == id:
-            found = state
-
-    return render_template(
-        "9-states.html",
-        state=found,
-    )
-
-
-def close_session(res):
-    """Close the current db session.
+def states(id=None):
+    """Handles the get request for /states and /states/<id>.
 
     Args:
-        res (obj): Response object.
+        id (str, optional): Id of the state to display. Defaults to None.
+
+    Returns:
+        str: Return from the render_template method.
+    """
+
+    state, states = None, None
+
+    if id:
+        state = storage.all(State).get('State.{}'.format(id), None)
+    else:
+        states = storage.all(State).values()
+
+    return render_template(
+        "9-states.html",
+        states=states,
+        state=state,
+    )
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Close the current db at the end of a request.
+
+    Args:
+        exception (Exception): Exception object.
     """
     storage.close()
 
-
-app.teardown_appcontext(close_session)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
